41328db85b77ca0f6e67399d88135689e3f06fe4
kairat <kairat95@gmail.com>
2022-02-10 23:09:28 +0600

    add test to home controller

---
+++ src/test/java/kz/spring/in/action/taco/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package kz.spring.in.action.taco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomeController() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+}
